7fded6f858a6baf94184ade16573097fcd321287
Pamela Ospina <pospina3@soy.sena.edu.co>
2024-04-05 17:01:23 -0500

    FRONT

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const exp = require('express');
 const app = exp()
+const path = require('path');
 
 const logger = require('morgan')
 app.use(logger('dev'));
+
+app.use(exp.static(path.join(__dirname, './static')));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './frontend/views'));
 
 app.use(exp.urlencoded({extended: false}));
 app.use(exp.json())
